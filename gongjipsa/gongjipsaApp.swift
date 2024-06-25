cbb39079c5825364beb514e6085f8b32e53b7dd0
김지하 <kimjiha1112@gmail.com>
2024-06-25 13:00:30 +0900

    Initial Commit

---
+++ gongjipsa/gongjipsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gongjipsaApp.swift
+//  gongjipsa
+//
+//  Created by Jihaha kim on 2024/06/25.
+//
+
+import SwiftUI
+
+@main
+struct gongjipsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gongjipsaUITests/gongjipsaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  gongjipsaUITestsLaunchTests.swift
+//  gongjipsaUITests
+//
+//  Created by Jihaha kim on 2024/06/25.
+//
+
+import XCTest
+
+final class gongjipsaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
