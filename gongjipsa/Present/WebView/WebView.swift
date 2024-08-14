09b93016b8a254d88cf54e36ff5d67661618c95d
Jiha Kim <kimjiha1112@gmail.com>
2024-08-14 22:52:56 +0900

    [#1] fix: NSURLErrorCancelled error (#16)
    * [#1] fix: NSURLErrorCancelled error
    
    * [#1] fix reservation external tap
    
    * [#1] fix wkwebview disable zoom

--- gongjipsa/Present/WebView/WebView.swift
+++ gongjipsa/Present/WebView/WebView.swift
@@ -14,24 +14,35 @@ struct WebView: UIViewRepresentable {
     @Binding var errorMessage: String?
     @ObservedObject var viewModel: WebViewModel
     
-        func makeUIView(context: Context) -> WKWebView {
-            let webView = FullScreenWKWebView()
-            webView.navigationDelegate = context.coordinator
-            webView.allowsBackForwardNavigationGestures = true
-            webView.allowsLinkPreview = true
-    
-            if !viewModel.cookies.isEmpty {
-                viewModel.setCookies(for: webView, cookies: viewModel.cookies)
-                    .sink { _ in
-                        webView.load(URLRequest(url: url))
-                    }
-                    .store(in: &viewModel.cancellables)
-            } else {
-                webView.load(URLRequest(url: url))
-            }
-    
-            return webView
+    func makeUIView(context: Context) -> WKWebView {
+        let source: String = "var meta = document.createElement('meta');" +
+        "meta.name = 'viewport';" +
+        "meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';" +
+        "var head = document.getElementsByTagName('head')[0];" +
+        "head.appendChild(meta);"
+        
+        let script: WKUserScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        let userContentController: WKUserContentController = WKUserContentController()
+        userContentController.addUserScript(script)
+        let conf = WKWebViewConfiguration()
+        conf.userContentController = userContentController
+        let webView = FullScreenWKWebView(frame: .zero, configuration: conf)
+        webView.navigationDelegate = context.coordinator
+        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsLinkPreview = true
+        
+        if !viewModel.cookies.isEmpty {
+            viewModel.setCookies(for: webView, cookies: viewModel.cookies)
+                .sink { _ in
+                    webView.load(URLRequest(url: url))
+                }
+                .store(in: &viewModel.cancellables)
+        } else {
+            webView.load(URLRequest(url: url))
         }
+        
+        return webView
+    }
     
     func updateUIView(_ webView: WKWebView, context: Context) {}
     
@@ -54,6 +65,10 @@ struct WebView: UIViewRepresentable {
         }
         
         func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            let nsError = error as NSError
+            if nsError.code == NSURLErrorCancelled {
+                return
+            }
             print("Failed provisional navigation with error: \(error)")
             parent.errorMessage = error.localizedDescription
         }
@@ -63,15 +78,16 @@ struct WebView: UIViewRepresentable {
                 decisionHandler(.cancel)
                 return
             }
-            
-            if isExternalURL(url) || url.scheme == "webcal" {
+            if isExternalURL(url) || url.scheme == "webcal" || url.absoluteString == "https://app.gongjipsa.com/reservations/all" {
                 UIApplication.shared.open(url)
                 decisionHandler(.cancel)
-            } else if isSafariURL(url) {
+            }
+            else if isSafariURL(url) {
                 parent.viewModel.showSafariScreen = true
                 parent.viewModel.SafariURL = url.absoluteString
                 decisionHandler(.cancel)
-            } else {
+            }
+            else {
                 decisionHandler(.allow)
             }
         }
